815485c11943be4a35e5a32a273bba345459b4e9
ny-a <58758248+ny-a@users.noreply.github.com>
2021-08-07 22:37:22 +0900

    Fix ffplay command

--- screen-share-play.sh
+++ screen-share-play.sh
@@ -6,7 +6,7 @@ LIVE_SERVER="${LIVE_SERVER:-rtmp://localhost/live}"
 STREAM_KEY="${STREAM_KEY:-screen}"
 
 while true; do
-  ffmpeg -hide_banner \
+  ffplay -hide_banner \
   -fflags nobuffer \
   -rw_timeout 1000000 \
   -f live_flv "${LIVE_SERVER}/${STREAM_KEY}"
