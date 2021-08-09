cc01a8e8ecca5c8f8268d584f5b82120ee2c3d9e
ny-a <58758248+ny-a@users.noreply.github.com>
2021-08-09 12:25:22 +0900

    Add BITRATE to camera-send

--- camera-send.sh
+++ camera-send.sh
@@ -5,12 +5,13 @@ set -eu
 LIVE_SERVER="${LIVE_SERVER:-rtmp://localhost/live}"
 STREAM_KEY="${PREVIEW_STREAM_KEY:-camera}"
 V4L2_INPUT="${V4L2_INPUT:-/dev/video10}"
+BITRATE="${BITRATE:-1M}"
 
 while true; do
   ffmpeg -hide_banner \
   -fflags nobuffer \
   -f v4l2 -i "$V4L2_INPUT" \
-  -c:v flv \
+  -c:v flv -b:v "$BITRATE" \
   -fflags flush_packets \
   -f fifo -fifo_format flv -map 0:v -drop_pkts_on_overflow 1 \
   -attempt_recovery 1 -recovery_wait_time 1 \
