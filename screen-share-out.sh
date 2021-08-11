1e00dc3f2481a71647ae37d0303c1eadef146101
ny-a <58758248+ny-a@users.noreply.github.com>
2021-08-11 23:27:30 +0900

    Add screen-share-out

---
+++ screen-share-out.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+set -u
+
+LIVE_SERVER="${LIVE_SERVER:-rtmp://localhost/live}"
+STREAM_KEY="${STREAM_KEY:-screen}"
+DISPLAY="${DISPLAY:-:0}"
+POSITION="${POSITION:-0,0}"
+VIDEO_SIZE="${VIDEO_SIZE:-1920x1080}"
+QUALITY="${QUALITY:-3}"
+V4L2_OUTPUT="${V4L2_OUTPUT:-/dev/video11}"
+
+echo $DISPLAY
+
+while true; do
+  ffmpeg -hide_banner \
+  -s "$VIDEO_SIZE" \
+  -f x11grab -i "${DISPLAY}+${POSITION}" \
+  -q "$QUALITY" -c:v flv -fflags flush_packets \
+  -f fifo -fifo_format flv -map 0:v -drop_pkts_on_overflow 1 \
+  -attempt_recovery 1 -recovery_wait_time 1 \
+  "${LIVE_SERVER}/${STREAM_KEY}" \
+  -f v4l2 "$V4L2_OUTPUT"
+done
