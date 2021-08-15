6d145486347ee77bc0b1f35dae4c42e65b8a41cc
ny-a <58758248+ny-a@users.noreply.github.com>
2021-08-15 12:36:17 +0900

    Add async to sound-out.sh

--- sound-out.sh
+++ sound-out.sh
@@ -10,8 +10,10 @@ AUDIO_OUTPUT="${AUDIO_OUTPUT:-headphone}"
 while true; do
   ffmpeg -hide_banner \
   -f alsa -i $AUDIO_INPUT \
+  -af 'aresample=async=1:min_hard_comp=0.100000:first_pts=0' \
   -f alsa $AUDIO_OUTPUT \
-  -c:a libmp3lame -fflags flush_packets \
+  -af 'aresample=async=1:min_hard_comp=0.100000:first_pts=0' \
+  -c:a aac -fflags flush_packets \
   -f fifo -fifo_format flv -map 0:a -drop_pkts_on_overflow 1 \
   -attempt_recovery 1 -recovery_wait_time 1 \
   "${LIVE_SERVER}/${PREVIEW_STREAM_KEY}"
