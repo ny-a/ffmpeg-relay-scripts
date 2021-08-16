6f405ce40e06afa0347e44670f26b417af2c34c5
ny-a <58758248+ny-a@users.noreply.github.com>
2021-08-17 01:33:12 +0900

    Remove async from sound-out.sh

--- sound-out.sh
+++ sound-out.sh
@@ -10,9 +10,7 @@ AUDIO_OUTPUT="${AUDIO_OUTPUT:-headphone}"
 while true; do
   ffmpeg -hide_banner \
   -f alsa -i $AUDIO_INPUT \
-  -af 'aresample=async=1:min_hard_comp=0.100000:first_pts=0' \
   -f alsa $AUDIO_OUTPUT \
-  -af 'aresample=async=1:min_hard_comp=0.100000:first_pts=0' \
   -c:a aac -fflags flush_packets \
   -f fifo -fifo_format flv -map 0:a -drop_pkts_on_overflow 1 \
   -attempt_recovery 1 -recovery_wait_time 1 \
