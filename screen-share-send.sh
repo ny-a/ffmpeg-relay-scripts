ef2bea7adcdd636c5b9b863f547fe3b45ba7c04e
ny-a <58758248+ny-a@users.noreply.github.com>
2021-08-21 20:11:01 +0900

    Remove group of picture from screen-share

--- screen-share-send.sh
+++ screen-share-send.sh
@@ -16,7 +16,7 @@ while true; do
   -f x11grab -i "${DISPLAY}+${POSITION}" \
   -c:v flv -b:v "$BITRATE" -fflags flush_packets \
   -flags low_delay -strict experimental \
-  -pix_fmt yuv420p -g 60 -preset ultrafast -tune zerolatency \
+  -pix_fmt yuv420p -preset ultrafast -tune zerolatency \
   -f fifo -fifo_format flv -map 0:v -drop_pkts_on_overflow 1 \
   -attempt_recovery 1 -recovery_wait_time 1 \
   $@ \
