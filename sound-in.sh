a96d8d298fac7d135d75e0d2472340af7e94301c
ny-a <58758248+ny-a@users.noreply.github.com>
2021-08-22 10:37:38 +0900

    Add OUTPUT_CODEC to sound-in/out

--- sound-in.sh
+++ sound-in.sh
@@ -5,6 +5,7 @@ set -u
 LIVE_SERVER="${LIVE_SERVER:-localhost}"
 STREAM_KEY="${STREAM_KEY:-sound-in}"
 INPUT_CODEC="${INPUT_CODEC:-pcm_s16le}"
+OUTPUT_CODEC="${OUTPUT_CODEC:-pcm_s16le}"
 AUDIO_INPUT="${AUDIO_INPUT:-intinsnoop}"
 AUDIO_OUTPUT="${AUDIO_OUTPUT:-loopout0mix}"
 
@@ -12,7 +13,7 @@ while true; do
   ffmpeg -hide_banner \
   -use_wallclock_as_timestamps 1 \
   -c:a "$INPUT_CODEC" -f alsa -i "$AUDIO_INPUT" \
-  -f alsa "$AUDIO_OUTPUT" \
+  -c:a "$OUTPUT_CODEC" -f alsa "$AUDIO_OUTPUT" \
   -strict experimental \
   -c:a aac -fflags flush_packets \
   -f fifo -fifo_format flv -map 0:a -drop_pkts_on_overflow 1 \
